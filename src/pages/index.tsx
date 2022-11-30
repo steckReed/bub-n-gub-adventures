51d4deb3be3d3d8256444b2bb1ec5bb7306ffb58
steckreed <steckreed@gmail.com>
2022-11-29 23:31:35 -0500

    Images now displaying

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { LogoJsonLd, NextSeo } from "next-seo";
 import { PageSEO } from "@Modules/SEO";
-import Image from 'next/image';
-import * as Imgs from '@Images/_index'
 import { motion } from 'framer-motion';
 import ImageContainer from '../components/modules/imageContainer/imageContainer';
 
@@ -17,53 +15,53 @@ export default function Home() {
   // SEO
 
   const imagesArr =[
-    '@Images/img-1.jpg',
-    '@Images/img-2.jpg',
-    '@Images/img-3.jpg',
-    '@Images/img-4.jpg',
-    '@Images/img-5.jpg',
-    '@Images/img-6.jpg',
-    '@Images/img-7.jpg',
-    '@Images/img-8.jpg',
-    '@Images/img-9.jpg',
-    '@Images/img-10.jpg',
-    '@Images/img-11.jpg',
-    '@Images/img-12.jpg',
-    '@Images/img-13.jpg',
-    '@Images/img-14.jpg',
-    '@Images/img-15.jpg',
-    '@Images/img-16.jpg',
-    '@Images/img-17.jpg',
-    '@Images/img-18.jpg',
-    '@Images/img-19.jpg',
-    '@Images/img-20.jpg',
-    '@Images/img-21.jpg',
-    '@Images/img-22.jpg',
-    '@Images/img-23.jpg',
-    '@Images/img-24.jpg',
-    '@Images/img-25.jpg',
-    '@Images/img-26.jpg',
-    '@Images/img-27.jpg',
-    '@Images/img-28.jpg',
-    '@Images/img-29.jpg',
-    '@Images/img-30.jpg',
-    '@Images/img-31.jpg',
-    '@Images/img-32.jpg',
-    '@Images/img-33.jpg',
-    '@Images/img-34.jpg',
-    '@Images/img-35.jpg',
-    '@Images/img-36.jpg',
-    '@Images/img-37.jpg',
-    '@Images/img-38.jpg',
-    '@Images/img-39.jpg',
-    '@Images/img-40.jpg',
-    '@Images/img-41.jpg',
-    '@Images/img-42.jpg',
-    '@Images/img-43.jpg',
-    '@Images/img-44.jpg',
-    '@Images/img-45.jpg',
-    '@Images/img-46.jpg',
-    '@Images/img-47.jpg'
+    '/images/img-1.jpg',
+    '/images/img-2.jpg',
+    '/images/img-3.jpg',
+    '/images/img-4.jpg',
+    '/images/img-5.jpg',
+    '/images/img-6.jpg',
+    '/images/img-7.jpg',
+    '/images/img-8.jpg',
+    '/images/img-9.jpg',
+    '/images/img-10.jpg',
+    '/images/img-11.jpg',
+    '/images/img-12.jpg',
+    '/images/img-13.jpg',
+    '/images/img-14.jpg',
+    '/images/img-15.jpg',
+    '/images/img-16.jpg',
+    '/images/img-17.jpg',
+    '/images/img-18.jpg',
+    '/images/img-19.jpg',
+    '/images/img-20.jpg',
+    '/images/img-21.jpg',
+    '/images/img-22.jpg',
+    '/images/img-23.jpg',
+    '/images/img-24.jpg',
+    '/images/img-25.jpg',
+    '/images/img-26.jpg',
+    '/images/img-27.jpg',
+    '/images/img-28.jpg',
+    '/images/img-29.jpg',
+    '/images/img-30.jpg',
+    '/images/img-31.jpg',
+    '/images/img-32.jpg',
+    '/images/img-33.jpg',
+    '/images/img-34.jpg',
+    '/images/img-35.jpg',
+    '/images/img-36.jpg',
+    '/images/img-37.jpg',
+    '/images/img-38.jpg',
+    '/images/img-39.jpg',
+    '/images/img-40.jpg',
+    '/images/img-41.jpg',
+    '/images/img-42.jpg',
+    '/images/img-43.jpg',
+    '/images/img-44.jpg',
+    '/images/img-45.jpg',
+    '/images/img-46.jpg',
+    '/images/img-47.jpg'
   ]
 
   return (
@@ -81,18 +79,15 @@ export default function Home() {
             // transition={{ repeat: Infinity, duration: 15 }}
           >
             
-            <div className="flex" style={{ width: '100vw' }}>
-              <Image
-                className='shadow4'
-                style={{ 
-                  borderRadius:"12px"
-                 }}
-                src={Imgs.Img1} // Route of the image file
-                // height={'auto'} // Desired size with correct aspect ratio
-                width={144} // Desired size with correct aspect ratio
-                alt="Your Name"
-              />
-              {/* <ImageContainer reqImg={imagesArr[5]}/> */}
+            <div className="flex-wrap" style={{ width: '100vw' }}>
+              <>
+                {imagesArr.map(element => {
+                  console.log(element)
+                  return (
+                    <ImageContainer reqImg={element} />
+                  )
+                })}
+              </>
             </div>
 
 
