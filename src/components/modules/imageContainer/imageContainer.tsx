70a2d26ef9f3e6e8aeb32aa8a9d363cfaec0e0d1
steckreed <crs209@zips.uakron.edu>
2026-03-31 10:23:28 -0400

    + migration of project onto firebase hosting

--- src/components/modules/imageContainer/imageContainer.tsx
+++ src/components/modules/imageContainer/imageContainer.tsx
@@ -15,9 +15,8 @@ export default function ImageContainer({ reqImg, i }){
         }} >
 
       <ImageWithFallback
-        // src={`/images/${reqImg}`}
-        src={`https://steckreed.github.io/bub-n-gub-adventures/images/${reqImg}`}
-        fallbackSrc={`https://bub-n-gub-adventures.vercel.app/images/${reqImg}`}
+        src={`/images/${reqImg}`}
+        fallbackSrc={`/images/${reqImg}`}
         height={200}
         width={250}
         alt='Your Name'
